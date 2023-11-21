487182e037e9272a80b3c88121d39c3f3271e0de
杨公周 <yanggz@dianzhng.com>
2023-11-21 12:08:28 +0800

    feat: fix lint

--- entry/src/main/js/default/components/self/self.js
+++ entry/src/main/js/default/components/self/self.js
@@ -1,24 +1,7 @@
 import Router from '@system.router'
 export default {
     data: {
-        taskList: [{
-            title: 'web H5 渲染',
-            uri: 'https://ishugui.com/download',
-            date: '2021-12-31 10:00:00'
-        }, {
-            title: '看电影',
-            date: '2021-12-31 20:00:00'
-        }],
-    },
 
-    setfinish(e) {
-        this.$element('testmarquee').start()
-    },
-    makestart(e) {
-        this.$element('testmarquee').start()
-    },
-    makestop(e) {
-        this.$element('testmarquee').stop()
     },
 
     webClick() {
